82b3c1eb60f59bdfa1c7333ea08f115a8307be85
Gina Sprint <sprint@gonzaga.edu>
2022-04-07 18:55:00 +0000

    finish ensemble notes

--- EnsembleFunS1/main.py
+++ EnsembleFunS1/main.py
@@ -28,8 +28,72 @@ y = ["False", "False", "True", "True", "True", "False", "True", "False", "True",
 table = [X[i] + [y[i]] for i in range(len(X))]
 
 # TODO: begin notes on ensemble learning here
+# ensemble learning: a collection of "weak" learners
+# that work together to make predictions
+# together they form a "stronger" learner
+# though there is no guarantee the ensemble is
+# any better than a single learner
+
+# ensemble classification: a collection of "weak"
+# classifiers that work together to make classifications
+# using some voting policy (simple majority voting for the project;
+# track record voting for the project bonus)
+
+# let N be the number of "weak" learners in our ensemble
+# example: N = 100, then we have 100 learners that work (vote)
+# together to make predictions
+# homegenous ensemble: all the learners are of the same type
+# example: N = 100 decision trees (called a random forest;
+# classification algorithm for the project)
+# heterogeneous ensemble: a mix of types
+
+# let M be the number of "better" learners from the N learners
+# that we retain to form our ensemble
+# M < N
+
+# what are some ways to generate N learners?
+# goal is to have some diversity amongst the learner
+# 1. generate a classifier (tree) using "different"
+# attribute subsets
+# 2. generate a classifier (tree) using "different"
+# training sets
+# 3. generate a classifier (tree) using "different"
+# attribute selection techniques
+# 4. others??? creative
+
+# lets start with #2.)
+# recall: we talked about 4 different ways to generate
+# train and test sets
+# 1. holdout method
+# 2. random subsampling
+# 3. k fold cross validation (and variants)
+# 4. bootstrap method
 
 # TODO: begin notes on bagging here
+# bagging: bootstrap aggregating
+# an ensemble approach to generating N trees
+# and choosing the best M from the N trees
+# (for the ensemble)
+# basic approach
+# 1. split your dataset into a test set and a "remainder set"
+# 2. using the remainder set, sample N (diff N 
+# than number of instances) bootstrap samples
+# and use each sample to build a tree
+# for each tree's sample:
+#   ~63.2% of instances will be sampled into training set
+#   ~36.8% of instances will not (form VALIDATION SET)
+# 3. measure the performance of the tree on the validation set
+# using a performance metric. then choose to retain the 
+# M best trees based on their performance scores... that is the ensemble
+# 4. using the best M trees, make predictions for each instance in
+# the test set (see step 1) using majority voting
+
+# advantages of bagging
+# 1. simple idea, simple to implement
+# 2. reduces overfitting
+# 3. generally increases accuracy
+# (reduces the classification variance across classifiers)
+
 
 # pasted from ClassificationFun (note this N is different from ensemble learning's use of N):
 # 4. bootstrap method
@@ -62,10 +126,23 @@ for row in sample:
     print(row)
 
 # TODO: begin notes on random attribute subsets here
+# returning to approach #1) from above (random subsets)
+# let F be the size of random attribute subsets
+# F >= 2
 
 # TODO: Ensemble Lab Task 2:
-# Define a python function that selects F random attributes from an attribute list
+# Define a python function that selects F random attributes
+# from an attribute list
 # (test your function with att_indexes (or header))
+def compute_random_subset(values, num_values):
+    # there is a function np.random.choice()
+    values_copy = values[:] # shallow copy
+    np.random.shuffle(values_copy) # in place shuffle
+    return values_copy[:num_values]
+F = 2
+print(compute_random_subset(header, F))
+att_indexes = list(range(len(header)))
+print(compute_random_subset(att_indexes, F))
 
 # project notes
 # implement a random forest (all learners are trees)
--- EnsembleFunS2/main.py
+++ EnsembleFunS2/main.py
@@ -28,8 +28,69 @@ y = ["False", "False", "True", "True", "True", "False", "True", "False", "True",
 table = [X[i] + [y[i]] for i in range(len(X))]
 
 # TODO: begin notes on ensemble learning here
+# ensemble learning: a collection of "weak" learners
+# that work together to make predictions
+# together, the learners from a "stronger" learner
+# though there is no guarantee that the ensemble
+# is "better" than any one learner
+
+# ensemble classification: a collection of "weak" classifiers
+# that work together to make classifications 
+# via voting strategy (simple majority voting for the project;
+# there is also weighted majority voting; track record voting
+# is bonus for the project)
+
+# let N be the number of "weak" learners in our ensemble
+# example N = 100, 100 weak learners that work together (vote)
+# to make classifications
+# homogeneous ensemble: all the learners are of the same type
+# example N = 100 decision trees (called a random forest;
+# what we will implement for project)
+# heterogenous ensemble: a mix of types
+
+# let M be the number of "better" learners from the N
+# "weak" learners that we retain to form our final ensemble
+# M < N
+
+# what are some ways to generate "weak" learners
+# goal is to have diversity
+# 1. generate a classifier (tree) using "different" training data
+# 2. generate a classifier (tree) using "different" attribute
+# selection techniques (random, entropy, gini, etc...)
+# 3. generate a classifier (tree) using "different" attribute
+# subsets
+# 4. others??? creative
+
+# recall: what are the techniques for train and test set
+# 1. holdout method**
+# 2. random subsampling
+# 3. k fold cross validation (and variants)
+# 4. bootstrap method**
 
 # TODO: begin notes on bagging here
+# bagging: bootstrap aggregating
+# an ensemble method for creating N weak learners
+# and retaining the M best learners to form an ensemble
+# basic approach
+# 1. divide the dataset into a test set and "remainder" set
+# 2. using the remainder set, sample N bootstrap samples
+# one for each of N trees (note this is a diff N than the sample size)
+# for each sample (used to build 1 tree):
+#   ~63.2% of instances will be sampled into training set
+#   ~36.8% of instances will not (form VALIDATION SET)
+# 3. measure the performance of each tree on its validation set
+# using some performance measure. retain the best M trees based
+# on the performance scores
+# 4. using majority voting, make M predictions from the M trees
+# for each unseen instance in the test set (the majority vote is the instance's
+# prediction)
+
+# advantages of bagging
+# 1. simple idea, simple to implement
+# 2. reduces overfitting
+# 3. typically improves accuracy
+# (reduces the variance across classifications)
+
 
 # pasted from ClassificationFun (note this N is different from ensemble learning's use of N):
 # 4. bootstrap method
@@ -62,10 +123,24 @@ for row in sample:
     print(row)
 
 # TODO: begin notes on random attribute subsets here
+# let's return to #3
+# let F be the size of random attribute subsets
+# F >= 2
+
 
 # TODO: Ensemble Lab Task 2:
-# Define a python function that selects F random attributes from an attribute list
+# Define a python function that selects F random attributes
+# from an attribute list
 # (test your function with att_indexes (or header))
+def compute_random_subset(values, num_values):
+    # you can use np.random.choice()
+    values_copy = values[:] # shallow copy
+    np.random.shuffle(values_copy) # in place
+    return values_copy[:num_values]
+F = 2
+print(compute_random_subset(header, F))
+att_indexes = list(range(len(header)))
+print(compute_random_subset(att_indexes, F))
 
 # project notes
 # implement a random forest (all learners are trees)
